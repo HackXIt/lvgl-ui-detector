cf118f769988e263ce2e72c86f3799d97520a742
HackXIt <hackxit@gmail.com>
2024-04-08 22:42:44 +0200

    Change upload scripts with usage of tags

--- scripts/upload_datasets.py
+++ scripts/upload_datasets.py
@@ -12,8 +12,8 @@ for yaml_file in yaml_files:
     # Extracting the base name (without .yaml extension)
     base_name = os.path.basename(yaml_file).replace('.yaml', '')
 
-    # Create a new dataset with the base name
-    dataset = Dataset.create(dataset_name=base_name, dataset_project='LVGL UI Detector')
+    # Get/Create a new dataset with the base name
+    dataset = Dataset.get(dataset_project='LVGL UI Detector', dataset_name=base_name, auto_create=True, dataset_tags=['lvgl-ui-detector'])
 
     # Add the corresponding folder and yaml file to the dataset
     folder_path = os.path.join(target_directory, base_name)
