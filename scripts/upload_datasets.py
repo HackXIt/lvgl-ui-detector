af3498561e10354357771bfb5aa58646a48a4be0
HackXIt <hackxit@gmail.com>
2024-04-08 17:08:58 +0200

    Update upload script for ClearML to work on all *.yaml datasets

---
+++ scripts/upload_datasets.py
@@ -0,0 +1,26 @@
+import os
+import glob
+from clearml import Dataset
+
+# Path to the target directory
+target_directory = os.path.join(os.path.curdir, "tmp")
+
+# Finding all *.yaml files in the target directory
+yaml_files = glob.glob(os.path.join(target_directory, '*.yaml'))
+
+for yaml_file in yaml_files:
+    # Extracting the base name (without .yaml extension)
+    base_name = os.path.basename(yaml_file).replace('.yaml', '')
+
+    # Create a new dataset with the base name
+    dataset = Dataset.create(dataset_name=base_name, dataset_project='LVGL UI Detector')
+
+    # Add the corresponding folder and yaml file to the dataset
+    folder_path = os.path.join(target_directory, base_name)
+    if os.path.exists(folder_path) and os.path.isdir(folder_path):
+        dataset.add_files(path=folder_path)
+    dataset.add_files(path=yaml_file)
+
+    # Upload and finalize the dataset
+    dataset.upload()
+    dataset.finalize()
